9696ae8888822f7e3f24e6a15adfa6cf9906527c
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2024-03-14 10:35:07 +0100

    install sass

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+// import Header from '../components/Header';
+import './Home.scss';
+
+
+function Home() {
+
+    return (
+        <div className="container">      
+            {/* <Header/>              */}
+        </div>
+    )
+}
+
+
+export default Home;

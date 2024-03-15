171fae78e264fbf271bd1eace4e6164433c2aefd
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2024-03-15 16:32:57 +0100

    About.js

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,4 +1,5 @@
 import MathildeBureau from "../img/about.jpg";
+import Bureau from "../img/skills-img5.jpg";
 import Pastille from "../img/pastille.svg";
 import Planner from "../img/dropdown.jpg";
 import Arrow from "../img/arrow.svg";
@@ -11,17 +12,18 @@ const About = () => {
         <section className="about" id="about">
 
             <div className="about_title">
-                <h2>Je suis à la recherche d’une nouvelle opportunité.</h2>
+                <h2>à la recherche d’une nouvelle opportunité.</h2>
             </div>
 
-            <div className="about_bonjour">
+            <div className="about_container">
 
-                <div className="about_container">
-                    <figure className="about_container-img">
-                        <img src={MathildeBureau} className="img"/>
-                        <img src={Pastille} className="pastille"/>
+                <div className="about_container-presentation">
+                    <figure className="about_container-presentation--img">
+                        <img src={MathildeBureau} className="img" alt="Photographie de Mathilde"/>
+                        <img src={Pastille} className="pastille" alt="Pastille"/>
+                        <button className="bouton bouton_bgNoir">Tech friendly</button>
                     </figure>
-                    <div className="about_container-txt">
+                    <div className="about_container-presentation--txt">
                         <p>
                             I’m Mathilde, an award winning french designer working worldwide and pushing 
                             boundaries of conceptual and innovative digital design.
@@ -35,49 +37,58 @@ const About = () => {
                         <p>
                             I’m Mathilde, an award winning french designer working worldwide and pushing boundaries of conceptual and innovative digital design.
                         </p>
-                        <a href="" className="bouton bouton_noir">contact</a>
                     </div>
+                </div>
+
+                <div className="about_container-presentation">   
+                    <div className="about_container-presentation--txt">
+                        <p>
+                            I’m Mathilde, an award winning french designer working worldwide and pushing 
+                            boundaries of conceptual and innovative digital design. Toujours impliquée dans mes missions, 
+                            j’aime découvrir et apprendre de nouveaux 
+                            langages.
+                        </p>
+                        <img src={Arrow} alt="Flèche"/>
+                    </div>
+                    <figure className="about_container-presentation--img">
+                        <img src={Bureau} className="img" alt="Photographie de Mathilde"/>
+                    </figure>
                 </div>
         
-                <div className="about_container">
-                    <div className="about_container-dropdown">
-                        <div className="about_container-dropdown--bloc">
-                            <div className="about_container-dropdown--bloc--title">
-                                <h3>How old are you ?</h3>
-                                <div className="more"></div>
-                            </div>
-                            <div className="about_container-dropdown--bloc--txt">
-                                <p>My extensive experience in front-end development empowers my design process to push user experience and interactions to the next level.  </p>
-                            </div>
+
+
+                <div className="dropdown">
+                    <div className="dropdown_bloc">
+                        <div className="dropdown_bloc-title">
+                            <h3>How old are you ?</h3>
+                            <div className="more"></div>
                         </div>
-                        <hr/>
-                        <div className="about_container-dropdown--bloc">
-                            <div className="about_container-dropdown--bloc--title">
-                                <h3>How old are you ?</h3>
-                                <div className="more"></div>
-                            </div>
-                            <div className="about_container-dropdown--bloc--txt">
-                                <p>My extensive experience in front-end development empowers my design process to push user experience and interactions to the next level.  </p>
-                            </div>
-                        </div>
-                        <hr/>
-                        <div className="about_container-dropdown--bloc">
-                            <div className="about_container-dropdown--bloc--title">
-                                <h3>How old are you ?</h3>
-                                <div className="more active"></div>
-                            </div>
-                            <div className="about_container-dropdown--bloc--txt active">
-                                <p>My extensive experience in front-end development empowers my design process to push user experience and interactions to the next level.  </p>
-                            </div>
+                        <div className="dropdown_bloc-txt">
+                            <p>My extensive experience in front-end development empowers my design process to push user experience and interactions to the next level.  </p>
                         </div>
                     </div>
-                    <figure className="about_container-dropdownImg">
-                        <img src={Planner}/>
-                    </figure>
-                    <figure className="about_container-dropdownArrow">
-                        <img src={Arrow}/>
-                    </figure>
-                </div>
+                    <hr/>
+                    <div className="dropdown_bloc">
+                        <div className="dropdown_bloc-title">
+                            <h3>How old are you ?</h3>
+                            <div className="more"></div>
+                        </div>
+                        <div className="dropdown_bloc-txt">
+                            <p>My extensive experience in front-end development empowers my design process to push user experience and interactions to the next level.  </p>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="dropdown_bloc">
+                        <div className="dropdown_bloc-title">
+                            <h3>How old are you ?</h3>
+                            <div className="more active"></div>
+                        </div>
+                        <div className="dropdown_bloc-txt active">
+                            <p>My extensive experience in front-end development empowers my design process to push user experience and interactions to the next level.  </p>
+                        </div>
+                    </div>
+                </div> 
+              
             </div> 
 
         </section>

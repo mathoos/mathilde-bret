c9e8e4ed07e6eada0e3a3d43779aa93d15a7e39d
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2024-03-21 12:20:51 +0100

    page-attente

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,10 @@
 import Header from '../sections/Header';
-import About from '../sections/About';
-import Skills from '../sections/Skills';
-import Projects from '../sections/Projects';
-// import Contact from '../sections/Contact';
-import './Home.scss';
-
 
 function Home() {
 
     return (
         <div>
             <Header/>
-            <About/>
-            <Skills/>
-            <Projects/>
-            {/* <Contact/> */}
         </div>   
     )
 }

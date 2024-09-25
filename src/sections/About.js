90655705add97871e0b8ca04c992241c8f4874e5
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2024-09-25 14:49:01 +0200

    rubber hose about

--- src/sections/About.js
+++ src/sections/About.js
@@ -7,8 +7,10 @@ import RollingStones from "../img/lessons-2.jpg";
 import HarleyDavidson from "../img/rolling-stones-tshirt.jpg";
 import AppareilPhoto from "../img/appareil-photo.png";
 import RubberWeb from "../img/rubber-web.svg";
+import RubberWebEyes from "../img/rubber-web-eyes.svg";
 import RubberWebStarOne from "../img/rubber-web-star-one.svg";
 import RubberWebStarTwo from "../img/rubber-web-star-two.svg";
+import Star from "../img/star.svg";
 import { Parallax } from "react-scroll-parallax";
 import { Reveal } from "react-awesome-reveal";
 import { bottomAnimation } from "../functions/keyframes";
@@ -242,22 +244,11 @@ const About = () => {
                             </div>
                             <figure className="bloc_img">
                                 <img src={RubberWeb} className="bloc_img-rubber" alt="Rubber Hose Web"/>
-                                <img src={RubberWebStarOne} className="bloc_img-starOne apparition2" alt="Rubber Hose Web"/>
-                                <img src={RubberWebStarTwo} className="bloc_img-starTwo apparition3" alt="Rubber Hose Web"/>
-                                <p className="bouton apparition3">Créative</p>
+                                <img src={Star} className="bloc_img-elements" alt="Rubber Hose Web"/>
+                                <img src={Star} className="bloc_img-elements star-2" alt="Rubber Hose Web"/>
                                 <p className="bouton bouton_2 apparition2">Sérieuse</p>
-                                {/* <svg className="bloc_img-traits hidden-mobile" width="255" height="216" viewBox="0 0 255 216" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M2.99945 90.4853C4.8438 84.2773 11.4283 76.8601 14.2967 71.424C25.7715 49.6775 36.3222 27.4337 46.1805 4.87924" stroke="#FFFBFB" strokeWidth="5" strokeLinecap="round"/>
-                                    <path d="M92.9015 132.422C109.171 106.664 132.83 85.197 153.404 63.2157" stroke="#FFFBFB" strokeWidth="5" strokeLinecap="round"/>
-                                    <path d="M127.402 193.664C139.301 189.079 159.559 190.287 171.31 189.056C198.118 186.249 224.913 183.389 251.751 180.926" stroke="#FFFBFB" strokeWidth="5" strokeLinecap="round"/>
-                                </svg> */}
-                                
-                                {/* <svg className="bloc_img-arrow" width="340" height="296" viewBox="0 0 340 296" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path className="path-animation" d="M231.775 124.261C242.628 131.138 255.171 130.254 266.266 125.96C275.444 122.408 283.903 117.425 292.841 113.421C294.173 112.824 290.976 115.688 290.793 115.89C286.304 120.836 282.789 126.331 280.307 132.511C277.057 140.603 276.02 149.255 277.892 158.013C278.859 162.535 280.584 166.969 283.084 170.977C285.568 174.961 288.908 177.739 292.389 180.876"/>
-                                    <path className="path-animation" d="M2.49535 168.453C7.54894 170.895 11.2153 174.692 16.0292 177.583C49.9999 197.987 81.9302 208.331 120.494 203.222C177.872 195.621 223.281 169.44 265.569 135.009"/>
-                                </svg> */}
-                            </figure>
-                           
+                                <p className="bouton apparition3">Créative</p>
+                            </figure>  
                         </div>
                     </div>
                 </div>

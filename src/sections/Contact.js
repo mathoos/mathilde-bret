52bb56f5f393567f6560120aa215dfba69cf5837
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2024-11-29 16:39:01 +0100

    animations

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,4 +1,5 @@
 import { Reveal } from "react-awesome-reveal";
+import { Parallax } from "react-scroll-parallax";
 import { bottomAnimationBloc} from "../functions/keyframes";
 import Rubber from "../img/rubber_head_sunglasses.svg";
 import './Contact.scss';
@@ -14,16 +15,23 @@ const Contact = () => {
                         <span>Let's get</span>
                         <span>in touch</span>
                     </h2>
-
+                    
                     <figure className="footer_title-img">
+                        <Parallax 
+                            translateY={['0px', '0px']}
+                            rotate={[5, 30]}
+                            className="bouton-parallax"
+                        >
                             <img src={Rubber} alt="Etoile" />
-                        </figure>
+                        </Parallax>
+                    </figure>
+                    
                 </div>
                 <div className="footer_links">
                     <a href="mailto:bretmathilde@orange.fr" className="bouton bouton_bgBlanc">Email</a>
                     <a href="tel:0663273524" className="bouton bouton_bgBlanc">Téléphone</a>
                     <a href="https://www.linkedin.com/in/mathildebret/" className="bouton bouton_bgBlanc" target="_blank" rel="noreferrer">LinkedIn</a>
-                    <a href="https://github.com/mathoos" className="bouton bouton_bgBlanc" target="_blank" rel="noreferrer">GitHub</a>
+                    <a href="https://github.com/mathoos" className="bouton bouton_blanc" target="_blank" rel="noreferrer">GitHub</a>
                 </div>
                 <div className="footer_copyright">
                     <p>Édition 2024</p>

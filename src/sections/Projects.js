cb17bd5b007d35464e77815061c33aa9e2cddaf0
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2025-01-08 10:43:24 +0100

    colors, projects, skills

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -38,13 +38,14 @@ const Projects = () => {
                                             <div className="description_txt">  
                                                 {item.text && item.text.map((text, textIndex) => (
                                                     <div key={textIndex} className="description_txt-bloc">
-                                                        <figure className="arrow">
+                                                        {/* <figure className="arrow">
                                                             <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                                 <path d="M2 23.833L23.4752 23.833" strokeWidth="4" strokeLinecap="square"/>
                                                                 <path d="M4.14844 4.14746L21.8646 21.8636" strokeWidth="4" strokeLinecap="square"/>
                                                                 <path d="M23.4746 2L23.4746 23.4752" strokeWidth="4" strokeLinecap="square"/>
                                                             </svg>
-                                                        </figure>
+                                                        </figure> */}
+                                                        <div className="circle"></div>
                                                         <p>
                                                             {text}
                                                         </p>
